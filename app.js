a04192d99477a8c0dff5fcb620a7605dd2e5debf
Puru <purugcit79@gmail.com>
2024-09-07 17:23:57 +0000

    cos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const cookieParser = require('cookie-parser')
 const logger = require('./logger');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes')
 const eventRouter = require('./routes/eventRoutes')
@@ -10,7 +11,9 @@ const paymentRouter = require('./routes/paymentRoutes')
 const viewRouter = require('./routes/viewRoutes')
 
 app.use(express.static(path.join(__dirname, 'views')))
-
+app.use(cors({
+  origin: 'http://localhost:3000' // Allow requests from your scanner app
+}));
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api/v1/users',userRouter)
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const winston = require('winston/lib/winston/config')
 /* Test DB connection */
 db.authenticate().then(() => console.log("SQLite connected")).catch(err => console.log('Error: '+err))
 
+
 /* Starting the port on port 4001. */
 const port = 4001
 app.listen(port, ()=>{
