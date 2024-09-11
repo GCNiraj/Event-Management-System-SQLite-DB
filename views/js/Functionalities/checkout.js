a6b7aa83a6c24dca216496da69960f7360520084
SpanNullDanger <spamrand71@gmail.com>
2024-09-11 09:49:17 +0000

    updated seats logic

--- views/js/Functionalities/checkout.js
+++ views/js/Functionalities/checkout.js
@@ -2,28 +2,29 @@ import { showAlert } from './alert.js'
 import { logout } from './logout.js'
 
 var obj
-try{
+let seats = Number.POSITIVE_INFINITY
+try {
     if (document.cookie) {
         var tokenString = document.cookie.split('; ').find(row => row.startsWith('token=')).split('=')[1];
         var obj = JSON.parse(tokenString);
         document.querySelector('#logout').addEventListener('click', (e) => logout())
-        document.querySelector('#photo').src = 'images/users/'+obj.photo
-        document.querySelector('#photo2').src = 'images/users/'+obj.photo
-        document.querySelector('#name').innerHTML = obj.name 
+        document.querySelector('#photo').src = 'images/users/' + obj.photo
+        document.querySelector('#photo2').src = 'images/users/' + obj.photo
+        document.querySelector('#name').innerHTML = obj.name
         document.querySelector('#email').innerHTML = obj.email
     } else {
         obj = JSON.parse('{}');
         showAlert('Unauthorized', 'You need to be valid user')
-        window.setTimeout(()=>{
+        window.setTimeout(() => {
             location.assign('/');
-        },1500)
+        }, 1500)
     }
-}catch (err){
+} catch (err) {
     showAlert('Unauthorized', 'You need to be valid user. Login to access this functionality')
     console.log(err)
-    window.setTimeout(()=>{
+    window.setTimeout(() => {
         location.assign('/');
-    },1500)
+    }, 1500)
 }
 
 const checkout = async (event_ID, attendee_CID, total_Amount, no_of_tickets) => {
@@ -40,9 +41,9 @@ const checkout = async (event_ID, attendee_CID, total_Amount, no_of_tickets) => 
         });
 
         if (res.data.status === 'success') {
+            updateAvailableSeats(seats-1)
             showAlert('success', 'Payment is successful');
             const transactionID = res.data.data.transaction_ID;
-            console.log(transactionID)
             const qrData = JSON.stringify({
                 transactionID,   // Only store minimal data, like transaction ID
                 attendee_CID,    // Attendee ID
@@ -63,11 +64,63 @@ const checkout = async (event_ID, attendee_CID, total_Amount, no_of_tickets) => 
 }
 
 
+const checkAvailableSeats = async () => {
+    try {
+        const res = await axios({
+            method: 'GET',
+            url: 'http://localhost:4001/api/v1/events/availability/' + localStorage.getItem('event_id')
+        });
+
+        if (res.data.status === 'success') {
+            if (res.data.data === null) {
+                console.log("Seat available")
+            }
+            else if (res.data.data !== null && res.data.data <= 0) {
+                throw new Error("Unavailable seats");
+            }
+            else if (res.data.data <= 0) {
+                throw new Error("Unavailable seats");
+            } else {
+                seats = res.data.data
+            }
+        }
+    } catch (err) {
+        let message = typeof err.response !== 'undefined' ? err.response.data.message : err.message;
+        showAlert('error', 'Error: Seat Unavailable', message);
+        location.assign('/')
+    }
+}
+
+window.onload = function () {
+    checkAvailableSeats();
+}
+
+const updateAvailableSeats = async (available_seats) => {
+    try {
+        const res = await axios({
+            method: 'PATCH',
+            url: 'http://localhost:4001/api/v1/events/availability/' + localStorage.getItem('event_id'),
+            data: {
+                available_seats
+            }
+        });
+
+        if (res.data.status === 'success') {
+            console.log("success")
+        }
+    } catch (err) {
+        console.log(err)
+        showAlert('error', 'Error: Seat Unavailable', message);
+    }
+}
+
+
 document.querySelector('#registerevent').addEventListener('click', (e) => {
     e.preventDefault()
     const total_Amount = parseInt(document.querySelector('.ttl-clr').textContent.match(/(\d+)/)[0])
     const event_ID = localStorage.getItem('event_id')
     const attendee_CID = obj["cid"]
     const no_of_tickets = parseInt(document.querySelector('.order-number').textContent)
+
     checkout(event_ID, attendee_CID, total_Amount, no_of_tickets)
 })
